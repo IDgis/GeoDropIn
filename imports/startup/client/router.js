2ee7e77ab1e065749540932a0868b985db643454
Sandro Neumann <sandro.neumann@idgis.nl>
2017-02-13 16:25:16 +0100

    check if user is authorized and if data can be found

--- imports/startup/client/router.js
+++ imports/startup/client/router.js
@@ -4,6 +4,8 @@ import { Router } from 'meteor/iron:router';
 import '/imports/ui/main.js';
 import '/imports/ui/list/list.js';
 import '/imports/ui/form/form.js';
+import '/imports/ui/unauthorized/unauthorized.js';
+import '/imports/ui/notfound/notfound.js';
 
 import { Geodata } from '/imports/api/collections/geodata.js';
 
@@ -28,8 +30,20 @@ Router.route('/form/add', function() {
 Router.route('/form/edit/:_id', function() {
 	Session.set('selectedGeodataId', this.params._id);
 	
-	var data = Geodata.findOne({_id: this.params._id});
-	this.render('form', {data: data});
+	var geodata = Geodata.findOne({_id: Session.get('selectedGeodataId')});
+	var user = Meteor.user();
+	
+	if(typeof geodata === 'undefined') {
+		this.render('notfound');
+	} else if(user !== null) {
+		if(geodata.user === user.username) {
+			var data = Geodata.findOne({_id: this.params._id});
+			this.render('form', {data: data});
+		} else {
+			this.render('unauthorized');
+		}
+	}
+	
 	}, {
 		name: 'formedit'
 });
