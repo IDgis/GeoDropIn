80159dbd8c0815058fa65a6fada06eef46b835e9
Sandro Neumann <sandro.neumann@idgis.nl>
2017-02-14 11:49:16 +0100

    added json page for datasets from last two weeks of specific user

--- imports/startup/server/router.js
+++ imports/startup/server/router.js
@@ -72,3 +72,34 @@ Router.route('/json/all/:user', function() {
 	
 	this.response.end(EJSON.stringify(json, {indent: true}));
 }, {where: 'server'});
+
+Router.route('/json/twoweeks/:user', function() {
+	var user = this.params.user;
+	var json = [];
+	
+	var dateTwoWeeksAgo = new Date();
+	dateTwoWeeksAgo.setDate(dateTwoWeeksAgo.getDate() - 14);
+	
+	var geodata = Geodata.find({user: user, lastRevisionDate: {$gte : dateTwoWeeksAgo, $lt: new Date()}});
+	geodata.forEach(function(item) {
+		var attUrls = [];
+		var cad = CouplingAttData.findOne({dataId: item._id});
+		
+		cad.attachmentIds.forEach(function(item) {
+			var attUrl = Meteor.absoluteUrl() + 'files/' + item;
+			attUrls.push(attUrl);
+		});
+		
+		var object = {_id: item._id, title: item.title, description: item.description, 
+						date: item.date, user: item.user, lastRevisionDate: item.lastRevisionDate,
+						attachmentIds: attUrls};
+		
+		json.push(object);
+	});
+	
+	this.response.writeHead(200, {
+	    'Content-Type': 'application/json; charset=UTF-8'
+	});
+	
+	this.response.end(EJSON.stringify(json, {indent: true}));
+}, {where: 'server'});
