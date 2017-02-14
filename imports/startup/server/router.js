6ed63d6944169535bae1f1809a8ba64b11fcd2a1
Sandro Neumann <sandro.neumann@idgis.nl>
2017-02-14 11:15:56 +0100

    added json page for all datasets of specific user

--- imports/startup/server/router.js
+++ imports/startup/server/router.js
@@ -1,7 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import { Router } from 'meteor/iron:router';
 
+import { Geodata } from '/imports/api/collections/geodata.js';
 import { Attachment } from '/imports/api/collections/attachment.js';
+import { CouplingAttData } from '/imports/api/collections/couplingAttData.js';
 
 Router.route('/files/:id', function() {
 	var fs = require('fs');
@@ -18,7 +20,8 @@ Router.route('/files/:id', function() {
 	    'Content-Type': contentType,
 	    'Content-Disposition': 'attachment; filename=' + filename,
 	    'Content-Length': data.length
-	  });
+	});
+	
 	this.response.write(data);
 	this.response.end();
 }, {where: 'server'});
@@ -27,12 +30,45 @@ Router.route('/logos/:filename', function() {
 	var fs = require('fs');
 	var path = require('path');
 	
-	var filePath = path.resolve('/GeoDropInLogos/' + this.params.filename);
+	var filename = this.params.filename;
+	
+	var filePath = path.resolve('/GeoDropInLogos/' + filename);
 	var data = fs.readFileSync(filePath);
     
 	this.response.writeHead(200, {
-	    'Content-Type': 'image/png'
-	  });
+	    'Content-Type': 'image/png',
+	    'Content-Disposition': 'attachment; filename=' + filename,
+	    'Content-Length': data.length
+	});
+	
 	this.response.write(data);
 	this.response.end();
+}, {where: 'server'});
+
+Router.route('/json/all/:user', function() {
+	var user = this.params.user;
+	var json = [];
+	
+	var geodata = Geodata.find({user: user});
+	geodata.forEach(function(item) {
+		var attUrls = [];
+		var cad = CouplingAttData.findOne({dataId: item._id});
+		
+		cad.attachmentIds.forEach(function(item) {
+			var attUrl = Meteor.absoluteUrl() + 'files/' + item;
+			attUrls.push(attUrl);
+		});
+		
+		var object = {_id: item._id, title: item.title, description: item.description, 
+						date: item.date, user: item.user, lastRevisionDate: item.lastRevisionDate,
+						attachmentIds: attUrls};
+		
+		json.push(object);
+	});
+	
+	this.response.writeHead(200, {
+	    'Content-Type': 'application/json; charset=UTF-8'
+	});
+	
+	this.response.end(EJSON.stringify(json, {indent: true}));
 }, {where: 'server'});
