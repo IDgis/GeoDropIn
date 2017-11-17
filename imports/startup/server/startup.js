7d8b9e7f83cd470bac7128d02ccc8b91636339d3
Sandro Neumann <sandro.neumann@idgis.nl>
2017-11-17 11:17:46 +0100

    Add runDockerImage method

--- imports/startup/server/startup.js
+++ imports/startup/server/startup.js
@@ -27,4 +27,68 @@ Meteor.startup(function () {
          
         return attempt.allowed;
     });
+    
+    var Future = Npm.require("fibers/future");
+    var exec = Npm.require("child_process").exec;
+   
+    Meteor.methods({
+    	runDockerImage: function (geodropinId, zipName, typeAction) {
+    		if(Meteor.user()) {
+				console.log('User is: ' + Meteor.user().username);
+    			
+    			if(Meteor.user().username === 'rijssenholten') {
+					var oracleUser = process.env.RIJSSENHOLTEN_ORACLE_DB_USER;
+					var oraclePassword = process.env.RIJSSENHOLTEN_ORACLE_DB_PASSWORD;
+				} else if(Meteor.user().username === 'kragten') {
+					var oracleUser = process.env.KRAGTEN_ORACLE_DB_USER;
+					var oraclePassword = process.env.KRAGTEN_ORACLE_DB_PASSWORD;
+				}
+			}
+    		
+    		if(typeof process.env.GEODROPIN_HOST !== 'undefined' &&
+    				typeof oracleUser !== 'undefined' &&
+    				typeof oraclePassword !== 'undefined' &&
+    				typeof zipName !== 'undefined' && zipName !== null &&
+    				typeof geodropinId !== 'undefined' && geodropinId !== null) {
+    			if(typeAction === 'insert') {
+        			this.unblock();
+        	        var future = new Future();
+        	        var command = "/usr/host/bin/docker run --rm " +
+        	        		"-e \"LD_LIBRARY_PATH=/opt/instantclient_12_1\" " +
+        	        		"-e \"TNS_ADMIN=/opt/instantclient_12_1\" " + 
+        	        		"-e \"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/instantclient_12_1\" " + 
+        	        		"-e \"JAVA_OPTS=-Xmx128M -Duser.timezone=Europe/Amsterdam\" " +
+        	        		"-e \"GEODROPIN_HOST=http://" + process.env.GEODROPIN_HOST + "\" " +
+        	        		"-e \"DB_IP=gdi_oracle_1\" " +
+        	        		"-e \"DB_PORT=1521\" " +
+        	        		"-e \"DB_SID=XE\" " +
+        	        		"-e \"DB_USER=" + oracleUser + "\" " +
+        	        		"-e \"DB_PASSWORD=" + oraclePassword + "\" " +
+        	        		"-e \"GEODATA_ZIP_NAME=" + zipName + "\" " +
+        	        		"-e \"GEODROPIN_ID=" + geodropinId + "\" " +
+        	        		"--volumes-from \"gdi_gdi.web_1\" " +
+        	        		"-v \"ogr2ogr_tnsadmin:/opt/instantclient_12_1\" " +
+        	        		"--network gdi-base " +
+        	        		"--link gdi_proxy_1:" + process.env.GEODROPIN_HOST + " " +
+        	        		"gdi_ogr2ogr.oracle.metadata " +
+        	        		"/opt/start.sh";
+        	        
+        	        exec(command, function(error, stdout, stderr){
+        	        	if(error) {
+        	        		console.log(error);
+        	        		throw new Meteor.Error(500, command + " failed");
+        	        	}
+        	        	
+        	        	future.return(stdout.toString());
+        	        });
+        	        
+        	        return future.wait();
+        		} else {
+        			console.log('docker run will not be executed');
+        		}
+    		} else {
+    			console.log('docker run will not be executed');
+    		}
+    	}
+    });
 });
